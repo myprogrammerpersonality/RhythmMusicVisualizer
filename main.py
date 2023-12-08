1ea37da1fe4a720f7c191c7f9c6c61bce48fbc4a
Ali_Yazdizadeh <aliyzd1379@gmail.com>
2023-12-08 19:37:32 +0330

    Add logging and timing to spectrogram plotting

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 import argparse
+import logging
+import time
 import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_spectrogram(file_path):
+def plot_spectrogram(file_path, sample_rate):
     # Load the audio file using librosa
-    y, sr = librosa.load(file_path, sr=None)
-
-    print("sr: ", sr)
+    start_time = time.time()
+    y, sr = librosa.load(file_path, sr=sample_rate)
+    logging.info(f"Loading audio took {time.time() - start_time} seconds")
 
     # Generate the spectrogram
+    start_time = time.time()
     D = librosa.amplitude_to_db(librosa.stft(y), ref=np.max)
+    logging.info(f"Generating spectrogram took {time.time() - start_time} seconds")
 
     # Display the spectrogram
     plt.figure(figsize=(10, 5))
+    start_time = time.time()
     librosa.display.specshow(D, sr=sr, x_axis='time', y_axis='log')
     plt.colorbar(format='%+2.0f dB')
     plt.title('Spectrogram of ' + file_path)
     plt.show()
+    logging.info(f"Displaying spectrogram took {time.time() - start_time} seconds")
 
 
 def main():
     parser = argparse.ArgumentParser(description="Audio Spectrogram Plotter")
     parser.add_argument("file_path", help="Path to the audio file")
+    parser.add_argument("--sample_rate", type=int, default=None, help="Sample rate for audio file")
 
     args = parser.parse_args()
 
-    plot_spectrogram(args.file_path)
+    plot_spectrogram(args.file_path, args.sample_rate)
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     main()
