b34409f77d9ad9eb7dce5f9cb1390f79099b820e
aliyzd1379 <aliyzd1379@gmail.com>
2024-08-21 13:02:52 +0330

    performance improve

---
+++ game_render_mp.py
@@ -0,0 +1,97 @@
+import sys
+import os
+import logging
+import argparse
+import pygame
+import librosa
+import multiprocessing as mp
+
+
+def render_frames(start, end, radii, width, height, output_dir):
+    pygame.init()
+    screen = pygame.Surface((width, height))
+    
+    for i in range(start, end):
+        screen.fill((0, 0, 0))
+        current_radius = radii[i]
+        pygame.draw.circle(screen, (255, 0, 0), (width // 2, height // 2), int(current_radius))
+        frame_filename = os.path.join(output_dir, f"frame_{i:05d}.png")
+        pygame.image.save(screen, frame_filename)
+    
+    pygame.quit()
+
+
+def main(args):
+    # Configure logger
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    window_size = args.sample_rate // args.fps
+
+    # Create an output directory for frames
+    output_dir = "frames"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+        logging.info("Created the frames folder!")
+
+    # Load music
+    y, sr = librosa.load(args.file_path, duration=args.duration, sr=args.sample_rate)
+    y_harm, y_perc = librosa.effects.hpss(y)
+
+    logging.info("Loaded the music.")
+
+    adj_val = 0
+    new_vals = []
+
+    for val in y_perc:
+        if val > 0:
+            adj_val += val
+        adj_val -= args.back_rate
+        if adj_val < 0:
+            adj_val = 0
+        new_vals.append(adj_val)
+
+    new_vals_compressed = []
+    i = 0
+    while i < len(new_vals):
+        val = max(new_vals[i:i+window_size])
+        new_vals_compressed.append(val * args.radius_scaling_factor)
+        i += window_size
+
+    logging.info(f"Processing Finished. len new vals: {len(new_vals_compressed)}")
+
+    # Set the number of processes
+    num_processes = mp.cpu_count() - 1
+
+    # Calculate chunk size
+    chunk_size = len(new_vals_compressed) // num_processes
+    processes = []
+
+    for i in range(num_processes):
+        start = i * chunk_size
+        end = (i + 1) * chunk_size if i != num_processes - 1 else len(new_vals_compressed)
+        p = mp.Process(target=render_frames, args=(start, end, new_vals_compressed, args.width, args.height, output_dir))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    logging.info("Rendering Finished!")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Render audio visualization frames using Pygame.")
+    parser.add_argument('--file_path', type=str, required=True, help='Path to the audio file')
+    parser.add_argument('--duration', type=int, default=60, help='Duration of audio to load in seconds')
+    parser.add_argument('--fps', type=int, default=60, help='Frames per second')
+    parser.add_argument('--sample_rate', type=int, default=44100, help='Sample rate of the audio')
+    parser.add_argument('--back_rate', type=float, default=0.01, help='Rate at which the adjustment value decreases')
+    parser.add_argument('--radius_scaling_factor', type=float, default=10, help='Scaling factor for the circle radius')
+    parser.add_argument('--width', type=int, default=800, help='Frames Width')
+    parser.add_argument('--height', type=int, default=600, help='Frames Height')
+
+    args = parser.parse_args()
+    main(args)
--- run.sh
+++ run.sh
@@ -4,17 +4,17 @@
 FRAME_RATE=60
 INPUT_FRAMES="frames/frame_%05d.png"
 START_TIME=0
-DURATION=60
+DURATION=120
 SAMPLE_RATE=44100
-BACK_RATE=0.01
+BACK_RATE=0.1
 RADIUS_FACTOR=10
-WIDTH=1280   # Set the desired width
-HEIGHT=720  # Set the desired height
-AUDIO_FILE="../data/be_soo.mp3"
-OUTPUT_VIDEO="outputs/output_video_60fps_audio.mp4"
+WIDTH=720   # Set the desired width
+HEIGHT=480  # Set the desired height
+AUDIO_FILE="../data/Taylor-Swift-Style-320.mp3"
+OUTPUT_VIDEO="outputs/Taylor-Swift-Style-320_60fps_480p_mp_batch.mp4"
 
 # Rendering with PyGame Command
-python game_render.py --file_path $AUDIO_FILE --duration $DURATION --fps $FRAME_RATE --sample_rate $SAMPLE_RATE --back_rate $BACK_RATE --radius_scaling_factor $RADIUS_FACTOR --width $WIDTH --height $HEIGHT
+python game_render_mp_batch.py --file_path $AUDIO_FILE --duration $DURATION --fps $FRAME_RATE --sample_rate $SAMPLE_RATE --back_rate $BACK_RATE --radius_scaling_factor $RADIUS_FACTOR --width $WIDTH --height $HEIGHT
 
 # Merging with FFmpeg Command
-ffmpeg -r $FRAME_RATE -i $INPUT_FRAMES -s ${WIDTH}x${HEIGHT} -ss $START_TIME -t $DURATION -i $AUDIO_FILE -c:v libx264 -vf fps=$FRAME_RATE -pix_fmt yuv420p -c:a aac -shortest $OUTPUT_VIDEO
+ffmpeg -r $FRAME_RATE -i $INPUT_FRAMES -ss $START_TIME -t $DURATION -i $AUDIO_FILE -c:v libx264 -vf fps=$FRAME_RATE -pix_fmt yuv420p -c:a aac -shortest $OUTPUT_VIDEO
